fa718c5c40af2876abf00a905873ea2c998eba5e
silas@2019 <mr.guosl@qq.com>
2020-01-17 16:51:16 +0800

    功能完善，添加签字，背景选择，预览状态优化

--- src/App.js
+++ src/App.js
@@ -1,45 +1,221 @@
 import React from 'react';
-import './App.css';
-import bg from './assets/img/heka-h5-bg.jpg';
+import './App.less';
+import bg2 from './assets/img/bg2.jpg';
+import bg3 from './assets/img/bg3.jpg';
+import bg4 from './assets/img/bg4.jpg';
+import { Button, Modal, Tabs, TextareaItem, List, Grid, ActivityIndicator } from 'antd-mobile';
+import html2canvas from 'html2canvas';
+import SignaturePad from './components/SignaturePad';
 
+function closest(el, selector) {
+  const matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+  while (el) {
+    if (matchesSelector.call(el, selector)) {
+      return el;
+    }
+    el = el.parentElement;
+  }
+  return null;
+}
 
-/**
-此刻
+// 背景图片数据
+const data = [
+  { icon: bg2, text: '恭贺新禧', signatureStyle: { top: '50.5%', left: '55%' }, textStyle: { top: '24.5%', left: '35%', color: '#dfc58d' } },
+  { icon: bg3, text: '福禄双全', signatureStyle: { top: '54.5%', left: '55%' }, textStyle: { top: '24.5%', left: '18.5%', color: '#edd1b1' } },
+  { icon: bg4, text: '你好,2020', signatureStyle: { top: '54.5%', left: '55%' }, textStyle: { top: '24.5%', left: '18.5%', color: '#ffe2c2' } }
+]
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.autoFocusInst = React.createRef();
+
+    this.state = {
+      ...this.state,
+      curBg: data[0]
+    }
+  }
+
+  state = {
+    displayText: `此刻
 伴随着新年钟声的敲响
 新的一年
 幸福开启
 真诚向您道一声
 谢谢
-祝您新的一年工作顺利 
- */
-class App extends React.Component {
+祝您新的一年工作顺利 `,
+    popupVisible: false,
+    previewModel: false,
+    previewImg: 'loading',
+    signatureImg: '',
+    penColor: ''
+  }
 
-  state = {
-    displayText: 'asdf'
+  tabs = [
+    { title: '文字', },
+    { title: '背景', },
+    { title: '签名' }
+  ];
+
+  showModal = (e) => {
+    e.preventDefault(); // 修复 Android 上点击穿透
+    const { curBg } = this.state;
+    this.setState({
+      popupVisible: true,
+      penColor: curBg.textStyle.color
+    });
+  }
+  onClose = () => {
+    this.setState({
+      popupVisible: false,
+    });
+  }
+
+  showPreviewModel = () => {
+    this.setState({
+      previewModel: true,
+    });
+  }
+
+  onPreviewModelClose = () => {
+    this.setState({
+      previewModel: false,
+    });
   }
 
   render() {
-    const { displayText } = this.state;
+    const { displayText, popupVisible, previewModel, previewImg, curBg, signatureImg, penColor } = this.state;
     return (
       <div className="App" >
-        <main className="app-container" >
-          <img src={bg} alt="bg" />
-          <div className="text-show-container">{displayText}</div>
+        <main className="app-container" id='capture'>
+          <img src={curBg.icon} alt="bg" />
+          <div className="text-show-container" style={curBg.textStyle}>{displayText}</div>
+
+          {
+            signatureImg &&
+            <div className="signature-kit" style={curBg.signatureStyle}>
+              <img src={signatureImg} alt="signatureImg" />
+            </div>
+          }
+
         </main>
-        <div className="app-tabs">
-          <textarea
-            onInput={(e) => { this.handleInput(e) }}
-            cols="30"
-            rows="2">
-          </textarea>
+
+        {/* 浮层按钮 */}
+        <div className="app-btnbar">
+          <i onClick={e => { this.showModal(e) }} className="iconfont icon-bianji"></i>
+          <i onClick={() => { this.handleCapture() }} className="iconfont icon-tubiao105"></i>
         </div>
+        <Modal
+          popup
+          onClose={this.onClose}
+          visible={popupVisible}
+          animationType="slide-up"
+          wrapProps={{ onTouchStart: this.onWrapTouchStart }}
+        >
+          <div style={{ height: '60vh' }}>
+            <Tabs tabs={this.tabs}
+              initialPage={0}
+            >
+              <div style={{ height: '200px', backgroundColor: '#fff' }}>
+                <List>
+                  <TextareaItem
+                    ref={this.autoFocusInst}
+                    value={displayText}
+                    onChange={val => { this.handleInput(val) }}
+                    autoHeight
+                  />
+                </List>
+              </div>
+              <div id='insert' style={{ backgroundColor: '#fff' }}>
+                <Grid
+                  data={data}
+                  columnNum={2}
+                  onClick={(el, index) => { this.selectBg(el, index) }}
+                />
+              </div>
+              <div style={{ backgroundColor: '#fff' }}>
+                <SignaturePad
+                  value={signatureImg}
+                  penColor={penColor}
+                  onUpdate={val => { this.updateSignature(val) }}></SignaturePad>
+              </div>
+            </Tabs>
+          </div>
+          <div className="edit-model-btns">
+            <Button type='primary' onClick={() => { this.onClose() }}>确认</Button>
+          </div>
+        </Modal>
+        <Modal
+          onClose={this.onPreviewModelClose}
+          visible={previewModel}
+          animationType="slide-up"
+          wrapProps={{ onTouchStart: this.onWrapTouchStart }}
+          actions={this.previewActions}
+        >
+          <div className="preview-container">
+            {previewImg === 'loading' ?
+              <ActivityIndicator size='large' animating />
+              :
+              <img src={previewImg} alt="preview" />
+            }
+          </div>
+          <p>提示:长按图片保存至本地或发送给朋友!</p>
+          <div className="preview-btns">
+            <Button type='primary' onClick={() => { this.onPreviewModelClose() }}>关闭</Button>
+          </div>
+        </Modal>
+
       </div>
     );
   }
 
-  handleInput(e) {
+  updateSignature(val) {
     this.setState({
-      displayText: e.target.value
+      signatureImg: val
+    })
+  }
+
+  selectBg(el, index) {
+    console.log(el, index);
+    this.setState({
+      curBg: el
+    })
+  }
+
+  handleCapture() {
+    this.showPreviewModel();
+    this.setState({
+      previewImg: 'loading'
+    })
+    setTimeout(() => {
+      html2canvas(document.querySelector("#capture")).then(canvas => {
+        var data = canvas.toDataURL('image/png', 1);  //1表示质量(无损压缩)
+        this.setState({
+          previewImg: data
+        })
+      });
+    }, 500)
+
+  }
+
+  onWrapTouchStart = (e) => {
+    // fix touch to scroll background page on iOS
+    if (!/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
+      return;
+    }
+    const pNode = closest(e.target, '.am-modal-content');
+    if (!pNode) {
+      e.preventDefault();
+    }
+  }
+
+
+
+
+  handleInput(val) {
+    this.setState({
+      displayText: val
     })
   }
 }
---
+++ src/components/SignaturePad/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Signature from 'signature_pad'
+import { Button, Toast } from 'antd-mobile';
+
+class SignaturePad extends React.Component {
+
+    componentDidMount() {
+        const { penColor, value } = this.props;
+
+        var wrapper = document.getElementById("signature-pad");
+        var canvas = wrapper.querySelector("canvas");
+        this.signaturePad = new Signature(canvas, {
+            // It's Necessary to use an opaque color when saving image as JPEG;
+            // this option can be omitted if only saving as PNG or SVG
+            backgroundColor: 'rgba(255, 255, 255,0)',
+            penColor: penColor || 'black'
+        });
+        if (value) {
+            this.signaturePad.fromDataURL(value)
+        }
+
+        // this.resizeCanvas(canvas);
+    }
+
+    resizeCanvas(canvas) {
+        // When zoomed out to less than 100%, for some very strange reason,
+        // some browsers report devicePixelRatio as less than 1
+        // and only part of the canvas is cleared then.
+        var ratio = Math.max(window.devicePixelRatio || 1, 1);
+
+        // This part causes the canvas to be cleared
+        canvas.width = canvas.offsetWidth * ratio;
+        canvas.height = canvas.offsetHeight * ratio;
+        canvas.getContext("2d").scale(ratio, ratio);
+
+        // This library does not listen for canvas changes, so after the canvas is automatically
+        // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
+        // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
+        // that the state of this library is consistent with visual state of the canvas, you
+        // have to clear it manually.
+        this.signaturePad.clear();
+    }
+
+    render() {
+        return (
+            <div id="signature-pad" className="signature-pad" onTouchMove={e => e.stopPropagation()}>
+                <div className="signature-pad--body">
+                    <canvas width='375' height='187.5'></canvas>
+                </div>
+                <div className="signature-pad--footer">
+                    <Button type='ghost' size='small' onClick={() => { this.dispatchAction('update') }}>更新签名</Button>
+                    <Button type='warning' size='small' onClick={() => { this.dispatchAction('clear') }} style={{ marginTop: '10px' }}>清除签名</Button>
+                    {/* <div className="description">Sign above</div>
+                    <div className="signature-pad--actions">
+                        <div>
+                            <button type="button" className="button clear" data-action="clear">Clear</button>
+                            <button type="button" className="button" data-action="change-color">Change color</button>
+                            <button type="button" className="button" data-action="undo">Undo</button>
+                        </div>
+                        <div>
+                            <button type="button" className="button save" data-action="save-png">Save as PNG</button>
+                            <button type="button" className="button save" data-action="save-jpg">Save as JPG</button>
+                            <button type="button" className="button save" data-action="save-svg">Save as SVG</button>
+                        </div>
+                    </div> */}
+                </div>
+            </div>
+        )
+    }
+
+    dispatchAction(action) {
+        const { onUpdate } = this.props;
+        let dataUrl = '';
+
+        if (this.signaturePad.isEmpty()) {
+            Toast.info('签名为空',1)
+            return;
+        }
+        if (action == 'update') {
+            dataUrl = this.signaturePad.toDataURL();
+            Toast.success('签名更新成功!',1);
+        } else if (action == 'clear') {
+            this.signaturePad.clear();
+            Toast.success('签名清除成功!',1);
+        }
+        onUpdate && onUpdate(dataUrl);
+    }
+}
+
+export default SignaturePad;
